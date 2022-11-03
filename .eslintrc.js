64dc949a46c9f5997bea06c483c82bdc65b34c15
Grigory Griko <griko1996@gmail.com>
2022-11-03 15:01:02 +0300

    feat: разворачивание сервера express

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "node": true,
+        "jest": true,
+        "mongo": true,
+        "es6": false
+      },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
