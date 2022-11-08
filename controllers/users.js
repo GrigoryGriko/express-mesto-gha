2ca74a4d5af9cfbde3840c6db3cd5c73d16e8c6d
Grigory Griko <griko1996@gmail.com>
2022-11-08 09:12:09 +0300

    feat: настройка роутов

---
+++ controllers/users.js
@@ -0,0 +1,27 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then(users => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+
+module.exports.getUserId = (req, res) => {
+  User.find({})
+    .then(users => {
+      if (!users[req.params.id]) {
+        res.send({ error: 'Такого пользователя нет' });
+      }
+      res.send(users[req.params.id]);
+    })
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+
+
+module.exports.createUsers = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then(user => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+}
