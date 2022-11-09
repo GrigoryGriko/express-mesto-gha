ac2176d8492cc1a1f5d575121c45bcb2d2ab2e54
Grigory Griko <griko1996@gmail.com>
2022-11-09 14:28:59 +0300

    feat: роуты настроены

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+
+const bodyParser = require('body-parser');
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({ extended: true }) );
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use('/users', usersRouter);
