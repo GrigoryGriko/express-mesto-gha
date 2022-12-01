470ad420d4aeaf3590cadcb33cde0c99ef007c04
Grigory Griko <griko1996@gmail.com>
2022-12-01 11:46:05 +0300

    feat: исправление контроллеров

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,78 +1,78 @@
 const User = require('../models/user');
 const NotFoundError = require('../NotFoundError');
+const { CODE_OK, CODE_BADREQUEST, CODE_SERVERERROR } = require('../constants/constants');
 
 module.exports.getAllUsersController = (req, res) => {
   User.find({})
-    .orFail(new NotFoundError(`Пользователи не найдены`))
-    .then(data => res.status(200).send({ data }))
+    .orFail(new NotFoundError('Пользователи не найдены'))
+    .then((data) => res.status(CODE_OK).send({ data }))
     .catch((err) => {
-      if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(400).send({ message: 'Переданы некорректные данные при получении пользователя' });
-      else if (err.name === 'NotFoundError') return res.status(err.errorCode).send({ message: err.errorMessage });
-      else return res.status(500).send({ message: 'Произошла ошибка' });
+      if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(CODE_BADREQUEST).send({ message: 'Переданы некорректные данные при получении пользователя' });
+      if (err.name === 'NotFoundError') return res.status(err.errorCode).send({ message: err.errorMessage });
+      return res.status(CODE_SERVERERROR).send({ message: 'Произошла ошибка' });
     });
-}
+};
 
 module.exports.getUserByIdController = (req, res) => {
-  console.log(req.params.userId);
-  User.findById(req.params.userId )
+  User.findById(req.params.userId)
     .orFail(new NotFoundError(`Пользователь с id '${req.params.userId}' не найден`))
-    .then(data => res.status(200).send({ data }))
+    .then((data) => res.status(CODE_OK).send({ data }))
     .catch((err) => {
-      if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(400).send({ message: 'Переданы некорректные данные при получении пользователя' });
-      else if (err.name === 'NotFoundError') return res.status(err.errorCode).send({ message: err.errorMessage });
-      else return res.status(500).send({ message: 'Произошла ошибка' });
+      if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(CODE_BADREQUEST).send({ message: 'Переданы некорректные данные при получении пользователя' });
+      if (err.name === 'NotFoundError') return res.status(err.errorCode).send({ message: err.errorMessage });
+      return res.status(CODE_SERVERERROR).send({ message: 'Произошла ошибка' });
     });
-}
+};
 
 module.exports.createUserController = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then(data => res.status(200).send({ data }))
+    .then((data) => res.status(CODE_OK).send({ data }))
     .catch((err) => {
-      if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(400).send({ message: 'Переданы некорректные данные при получении пользователя' });
-      else  return res.status(500).send({ message: 'Произошла ошибка' });
+      if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(CODE_BADREQUEST).send({ message: 'Переданы некорректные данные при получении пользователя' });
+      return res.status(CODE_SERVERERROR).send({ message: 'Произошла ошибка' });
     });
-}
+};
 
 module.exports.updateProfileController = (req, res) => {
-  const { name, about } = req.body;
+  const { Name, About } = req.body;
   User.findByIdAndUpdate(
     req.user._id,
-    { 
-      name: name,
-      about: about
+    {
+      name: Name,
+      about: About,
     },
 
-    { 
+    {
       new: true,
-      runValidators: true
-    }
+      runValidators: true,
+    },
   )
-  .orFail(new NotFoundError(`Пользователь с id '${req.params.userId}' не найден`))
-    .then(data => res.status(200).send({ data }))
+    .orFail(new NotFoundError(`Пользователь с id '${req.params.userId}' не найден`))
+    .then((data) => res.status(CODE_OK).send({ data }))
     .catch((err) => {
-      if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(400).send({ message: 'Переданы некорректные данные при получении пользователя' });
-      else if (err.name === 'NotFoundError') return res.status(err.errorCode).send({ message: err.errorMessage });
-      else return res.status(500).send({ message: 'Произошла ошибка' });
+      if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(CODE_BADREQUEST).send({ message: 'Переданы некорректные данные при получении пользователя' });
+      if (err.name === 'NotFoundError') return res.status(err.errorCode).send({ message: err.errorMessage });
+      return res.status(CODE_SERVERERROR).send({ message: 'Произошла ошибка' });
     });
-}
+};
 
 module.exports.updateAvatarController = (req, res) => {
-  const { avatar } = req.body;
+  const { Avatar } = req.body;
   User.findByIdAndUpdate(
     req.user._id,
-    { avatar: avatar },
+    { avatar: Avatar },
 
-    { 
+    {
       new: true,
-      runValidators: true
-    }
+      runValidators: true,
+    },
   )
-    .then(data => res.status(200).send({ data }))
+    .then((data) => res.status(CODE_OK).send({ data }))
     .catch((err) => {
-      if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(400).send({ message: 'Переданы некорректные данные при получении пользователя' });
-      else if (err.name === 'NotFoundError') return res.status(err.errorCode).send({ message: err.errorMessage });
-      else return res.status(500).send({ message: 'Произошла ошибка' });
+      if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(CODE_BADREQUEST).send({ message: 'Переданы некорректные данные при получении пользователя' });
+      if (err.name === 'NotFoundError') return res.status(err.errorCode).send({ message: err.errorMessage });
+      return res.status(CODE_SERVERERROR).send({ message: 'Произошла ошибка' });
     });
-}
+};
