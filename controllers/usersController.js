9e92eb6e2d44447f08b11ca2d6935aeb5b0a8e6c
Grigory Griko <griko1996@gmail.com>
2022-12-01 10:12:26 +0300

    feat: исправление контроллеров

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -28,11 +28,9 @@ module.exports.createUserController = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .orFail(new NotFoundError('объект не найден'))
     .then((data) => res.status(CODE_OK).send({ data }))
     .catch((err) => {
       if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(CODE_BADREQUEST).send({ message: 'Переданы некорректные данные при получении пользователя' });
-      if (err.name === 'NotFoundError') return res.status(err.errorCode).send({ message: err.errorMessage });
       return res.status(CODE_SERVERERROR).send({ message: 'Произошла ошибка' });
     });
 };
