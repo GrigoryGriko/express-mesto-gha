dff91bf1fa7710d46ddcf5ff8f650fb4af45e25e
Grigory Griko <griko1996@gmail.com>
2022-12-01 09:15:24 +0300

    feat: исправлена ошибка в файле констант

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,11 @@
-const CODE_OK = module.exports = 200;
-const CODE_BADREQUEST = module.exports = 400;
-const CODE_NOTFOUND = module.exports = 404;
-const CODE_SERVERERROR = module.exports = 500;
+const CODE_OK = 200;
+const CODE_BADREQUEST = 400;
+const CODE_NOTFOUND = 404;
+const CODE_SERVERERROR = 500;
+
+module.exports = {
+  CODE_OK,
+  CODE_BADREQUEST,
+  CODE_NOTFOUND,
+  CODE_SERVERERROR,
+};
