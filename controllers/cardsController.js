edb951c486f774eef52955742c121f1356cf67eb
Grigory Griko <griko1996@gmail.com>
2022-12-01 10:37:10 +0300

    feat: исправление контроллеров

--- controllers/cardsController.js
+++ controllers/cardsController.js
@@ -4,11 +4,9 @@ const { CODE_OK, CODE_BADREQUEST, CODE_SERVERERROR } = require('../constants/con
 
 module.exports.getAllCardsController = (req, res) => {
   Card.find({})
-    .orFail(new NotFoundError('Карточки не найдены'))
     .then((data) => res.status(CODE_OK).send({ data }))
     .catch((err) => {
       if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(CODE_BADREQUEST).send({ message: 'Переданы некорректные данные при получении карточки' });
-      if (err.name === 'NotFoundError') return res.status(err.errorCode).send({ message: err.errorMessage });
       return res.status(CODE_SERVERERROR).send({ message: 'Произошла ошибка' });
     });
 };
@@ -17,11 +15,9 @@ module.exports.createCardController = (req, res) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .orFail(new NotFoundError('Объект не найден'))
     .then((data) => res.status(CODE_OK).send({ data }))
     .catch((err) => {
       if (err.name === 'ValidationError' || err.name === 'CastError' || err.name === 'BadRequest') return res.status(CODE_BADREQUEST).send({ message: 'Переданы некорректные данные при создании карточки' });
-      if (err.name === 'NotFoundError') return res.status(err.errorCode).send({ message: err.errorMessage });
       return res.status(CODE_SERVERERROR).send({ message: 'Произошла ошибка' });
     });
 };
