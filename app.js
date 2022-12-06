2d6484b407c68c2131cdd3235584b4ce85f3f084
Grigory Griko <griko1996@gmail.com>
2022-12-06 12:01:03 +0300

    feat: исправление мелких ошибок

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const usersRouter = require('./routes/usersRouter');
 const cardsRouter = require('./routes/cardsRouter');
 const CODE_NOTFOUND = require('./constants/constants');
@@ -10,7 +9,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json().urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
