f0138514194ea49b73e7f4ae2ae84d078148fb49
Grigory Griko <griko1996@gmail.com>
2022-12-06 13:08:55 +0300

    feat: настроена ошибка 404

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const usersRouter = require('./routes/usersRouter');
 const cardsRouter = require('./routes/cardsRouter');
-const CODE_NOTFOUND = require('./constants/constants');
+const { CODE_NOTFOUND } = require('./constants/constants');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(express.json());
-app.use(express.json().urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
