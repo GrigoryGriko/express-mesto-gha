84e1fe4a1a8c3a3a7c96c7b1e342a38be0d160fc
Grigory Griko <griko1996@gmail.com>
2023-02-16 16:27:53 +0300

    feat: проверка на уникальность email работает

--- app.js
+++ app.js
@@ -22,12 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.post('/signin', validateUserLogin, login);
 app.post('/signup', validateUserRegister, createUser);
--- models/user.js
+++ models/user.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    unigue: true,
+    unique: true,
     required: true,
     validate: {
       validator: (v) => validator.isEmail(v),
