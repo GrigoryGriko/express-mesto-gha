44919bd7e7f800ef126d755ff64e0c01a76224a2
Grigory Griko <griko1996@gmail.com>
2023-02-16 16:11:57 +0300

    feat: написать проверку email на уникальность на уровне mongoose

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.post('/signin', validateUserLogin, login);
 app.post('/signup', validateUserRegister, createUser);
