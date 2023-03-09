7379c9a9c05197d564d7f29eb526bc1ee91b617e
Grigory Griko <griko1996@gmail.com>
2023-03-09 09:31:09 +0300

    fix: роуты вынесены в index.js

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
-const usersRouter = require('./routes/usersRouter');
-const cardsRouter = require('./routes/cardsRouter');
-const auth = require('./middlewares/auth');
-const { CODE_NOTFOUND } = require('./constants/constants');
-
-const errorHandler = require('./middlewares/errorHandler');
-const {
-  validateUserLogin,
-  validateUserRegister,
-} = require('./middlewares/validation');
-const {
-  login,
-  createUser,
-} = require('./controllers/usersController');
+const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
@@ -27,20 +13,7 @@ app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.post('/signin', validateUserLogin, login);
-app.post('/signup', validateUserRegister, createUser);
-
-app.use(auth);
-
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
-
-app.use(errors());
-app.use(errorHandler);
-
-app.use((req, res) => {
-  res.status(CODE_NOTFOUND).send({ message: 'Данный ресурс не найден' });
-});
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Сервер работает (порт: ${PORT})`);
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { errors } = require('celebrate');
+
+const errorHandler = require('../middlewares/errorHandler');
+const usersRouter = require('./usersRouter');
+const cardsRouter = require('./cardsRouter');
+const auth = require('../middlewares/auth');
+const NotFoundError = require('../errors/NotFoundError');
+const {
+  validateUserLogin,
+  validateUserRegister,
+} = require('../middlewares/validation');
+const {
+  login,
+  createUser,
+} = require('../controllers/usersController');
+
+router.post('/signin', validateUserLogin, login);
+router.post('/signup', validateUserRegister, createUser);
+router.use(auth);
+router.use('/users', usersRouter);
+router.use('/cards', cardsRouter);
+
+router.use(errors());
+router.use(errorHandler);
+
+router.use((req, res, next) => {
+  next(new NotFoundError('Данный ресурс не найден'));
+});
+
+module.exports = router;
