8caf9b1c6e08e344b70cb8a6391bbcfedcbd2fc6
Grigory Griko <griko1996@gmail.com>
2023-03-09 09:47:19 +0300

    fix: роуты вынесены в index.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
 const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
@@ -9,7 +8,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
